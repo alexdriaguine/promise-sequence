3e7ae3cf1bd250097450d1f5a6317fc65a997395
alexdriaguine <alexdriagin12@gmail.com>
2017-05-17 07:03:45 +0200

    refactor: pointfree promise then/catch

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
+const appendError = ({ignoreErrors, res, err}) =>
+  ignoreErrors ? res : [...res, err]
+
+const appendValue = ({res, current}) => [...res, current]
+
 function trainflow({
-  promiseFns,
+  fns,
   ignoreErrors = true,
   onResolveCurrent = val => {},
   onFinished = () => {}
 }) {
-  const max = promiseFns.length
+  const max = fns.length
   let count = 0
 
-  const appendError = (results, err) =>
-    ignoreErrors ? results : [...results, err]
-  const appendValue = (results, val) => [...results, val]
-  const runPromiseFunc = (promiseFn, results = []) => {
+  const run = (fn, res = []) => {
     const isDone = ++count === max
-    return promiseFn()
-      .then(current => {
-        const all = appendValue(results, current)
-        onResolveCurrent({current, all})
-        return isDone ? all : runPromiseFunc(promiseFns[count], all)
-      })
-      .catch(
-        err =>
-          isDone
-            ? appendError(results, err)
-            : runPromiseFunc(promiseFns[count], appendError(results, err))
-      )
+    const appendValue = (res, val) => [...res, val]
+    const handleError = err =>
+      isDone
+        ? appendError({ignoreErrors, res, err})
+        : run(
+            fns[count],
+            appendError({ignoreErrors, res, err})
+          )
+
+    const handleSuccess = current => {
+      const all = appendValue({res, current})
+      onResolveCurrent({current, all})
+      return isDone ? all : run(fns[count], all)
+    }
+
+    return fn().then(handleSuccess).catch(handleError)
   }
 
-  return runPromiseFunc(promiseFns[0])
+  return run(fns[0])
 }
 
 module.exports = trainflow
