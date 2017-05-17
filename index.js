4d5ff639902ffa6b71b1575d8d3543af52dd6ee8
alexdriaguine <alexdriagin12@gmail.com>
2017-05-17 07:06:26 +0200

    refactor

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const appendError = ({ignoreErrors, res, err}) =>
-  ignoreErrors ? res : [...res, err]
-
-const appendValue = ({res, current}) => [...res, current]
-
 function trainflow({
   fns,
   ignoreErrors = true,
@@ -15,14 +10,11 @@ function trainflow({
   const run = (fn, res = []) => {
     const isDone = ++count === max
     const appendValue = (res, val) => [...res, val]
-    const handleError = err =>
-      isDone
-        ? appendError({ignoreErrors, res, err})
-        : run(
-            fns[count],
-            appendError({ignoreErrors, res, err})
-          )
+    const appendError = (res, err) => (ignoreErrors ? res : [...res, err])
 
+    const handleError = err =>
+      isDone ? appendError(res, err) : run(fns[count], appendError(res, err))
+      
     const handleSuccess = current => {
       const all = appendValue({res, current})
       onResolveCurrent({current, all})
